20d1b116de65581426e389dbf903dd2cccd75c5f
Mattan-Sania <131472426+Mattan-Sania@users.noreply.github.com>
2025-10-07 15:18:55 +0200

    main_go.py

--- main_go.py
+++ main_go.py
@@ -1,26 +1,33 @@
 import sys
 import random
-random.seed(42)  # reproductibilité pour les intérêts aléatoires
 
 import networkx as nx
 from matplotlib import pyplot as plt
 
 from reco_interest import reco_interests_go
 from reco_link import reco_link_go
+from metrics import modularity, get_metrics, moyenne_sur_essais, print_metrics
 
-# 1) Graphe de base non orienté puis conversion en DiGraph (symétrisation des arêtes)
-G0_undirected = nx.random_partition_graph([7]*3, 0.3, 0.25, seed=42)
-G0 = nx.DiGraph()
-G0.add_nodes_from(G0_undirected.nodes())
-G0.add_edges_from((u, v) for u, v in G0_undirected.edges())
-G0.add_edges_from((v, u) for u, v in G0_undirected.edges())
+#________________________________________________________________________________________________________________
+
+N = 30
+init, liens, interets = moyenne_sur_essais(N)
+
+# 1) Graphe de base non orienté 
+G0 = nx.planted_partition_graph(3, 10, 0.3, 0.25, seed=42, directed=True)
 
 # 2) Recommandations orientées avec seuils réajustés
-G_liens = reco_link_go(G0, seuil=3)                 # un peu plus strict que 2
+G_liens = reco_link_go(G0, seuil=3)                 
 G_inter = reco_interests_go(G0, seuil=0.35, d=12)   # plus permissif pour des vecteurs 0/1 aléatoires
 
+
+print(f"\n Moyenne sur {N} essais")
+print_metrics("Graphe initial", init)
+print_metrics("Recommandation par liens", liens)
+print_metrics("Recommandation par interets", interets)
+
 # 3) Mise en page partagée
-pos = nx.spring_layout(G0_undirected, seed=42, k=0.3)
+pos = nx.spring_layout(G0, seed=42, k=0.3)
 
 # 4) Affichage
 fig, axes = plt.subplots(1, 3, figsize=(15, 5))
